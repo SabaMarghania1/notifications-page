a2c4019f042704892adc41fdc2d7e9c98a4edb57
Saba Marghania <marghanias@gmail.com>
2024-02-16 00:33:07 +0400

    test again

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -3,7 +3,11 @@ import styles from "./Messages.module.css";
 function Messages(props) {
   console.log(props.messages[0]);
 
-  return <ul className={styles.list}></ul>;
+  return (
+    <ul className={styles.list}>
+      <img src={props.messages[0].img} alt="" />
+    </ul>
+  );
 }
 
 export default Messages;
